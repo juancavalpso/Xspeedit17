31d147e37f1aabdccc460355e4277c9f470dc8b9
jcastudillocabezas <jcastudillocabezas@sqli.com>
2018-06-21 14:57:48 +0200

    Add existing file

--- src/main/java/com/hello/koding/HelloController.java
+++ src/main/java/com/hello/koding/HelloController.java
@@ -3,6 +3,7 @@ package com.hello.koding;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,17 +18,43 @@ public class HelloController {
         return "hello";
     }
 
-    @RequestMapping("/test")
-    public String test(){
-        return  separa(52364, "") ;
+    /**
+     * Ejemplo : http://localhost:8080/test/163841689525773
+     *
+     * @param n
+     * @return
+     */
+    @RequestMapping("/test/{n}")
+    public int[] test(@PathVariable long n){
+
+        /* Separamos cadena de manera recursiva */
+        String[] arr = separa(n, "").split("") ;
+        /* convertimos la cadena a int */
+        int[] numeros = new int[arr.length];
+        for (int i=0; i< arr.length; i++){
+            numeros[i]=Integer.valueOf(arr[i]);
+        }
+
+        double i = sumOfArray(numeros,numeros.length-1);
+
+        return numeros;
     }
 
-    private static String separa(int n, String r){
+    public double sumOfArray(int[] a, int n) {
+        if (n == 0)
+            return a[n];
+        else
+            return a[n] + sumOfArray(a, n-1);
+    }
+
+    private static String separa(long n, String r){
         if( n < 10 )
             return r;
         else if ( n/10 >= 1000 && r.equals(""))
-            return separa(n, " "+n%10);
+            return separa(n, ""+n%10);
         else
-            return separa(n/10, " "+(((n/10))%10) + r );
+            return separa(n/10, ""+(((n/10))%10) + r );
     }
+
+
 }
